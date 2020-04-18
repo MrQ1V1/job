52fedb889ff632f467f7aadbf3ffa62d152a4006
Mes'yebur <Aleksey.zobnin1996@yandex.ru>
2020-04-19 01:40:47 +0700

    Add files via upload
    PizzАвоз

---
+++ pizzaclient.lua
@@ -0,0 +1,191 @@
+﻿addEvent("openinf_pizza", true)
+screenWidth, screenHeight = guiGetScreenSize()
+function windm ()
+	if not getElementData ( localPlayer, "pizza_working") then
+		  infowind = guiCreateStaticImage(screenWidth - 700, screenHeight - 500, 300, 359, "background.png", false)
+		  
+		  guiMemoSetReadOnly( infotext, true )
+		  --guiSetFont(text2, "default-bold-small") 
+		  showCursor(true)
+		  guiSetVisible ( infowind , true) 
+		  guiWindowSetSizable(infowind, false) 
+		  Button_Glose = guiCreateStaticImage(25, 300, 250, 45, "bu2.png", false, infowind)
+		  Button_Start = guiCreateStaticImage(25, 250, 250, 45, "bu.png", false, infowind)
+		 
+		  addEventHandler("onClientGUIClick", Button_Start, kit )
+			addEventHandler("onClientGUIClick", Button_Glose, noshow )
+	else
+		for i, v in ipairs ( createdPickups ) do
+			if isElement ( v ) then
+				local pick2 = getElementData ( v, "pick2")
+				if isElement ( pick2 ) then
+					destroyElement ( pick2 )
+				end
+				local icon = getElementData ( v, "icon" )
+				if isElement ( icon ) then
+					destroyElement ( icon )
+				end
+				destroyElement ( v )
+			end
+		end
+		triggerServerEvent ( "finitoWork_pizza", localPlayer )
+		setElementData ( localPlayer, "pizza_working", false )
+		outputChatBox ( "Вы закончили работу разносчика пиццы", 255, 255, 255, true )
+	end
+end
+addEventHandler("openinf_pizza", root, windm )
+function noshow ()
+ if ( source == Button_Glose ) then
+   destroyElement ( infowind )
+   showCursor ( false ) 
+ end
+end
+function kit ()
+ if ( source == Button_Start ) then
+	startWorking()
+    destroyElement ( infowind )
+    showCursor ( false )
+  end 
+end
+
+addEvent ( "pay", true )
+function giving ()
+ outputChatBox ( "Вы собрали мусор и получили - 50$ ", getRootElement(), 255, 100, 100, true )
+end
+addEventHandler("pay", resourceRoot, giving )
+addEvent ( "get", true )
+function peremen (plr)
+ plr = getLocalPlayer ()
+end
+addEventHandler("get", resourceRoot, peremen )
+
+pickupSpawns = {
+	{ 2225,2520,10 },
+    { 2102,2484,11 },
+    { 1889,2408,11 },
+    { 1934,2306,10 },
+	{ 2065,2290,10 },
+	{ 2279,2341,10 },
+	{ 2258,2289,10 },
+	{ 2497,2239,10 },
+	{ 2519,2335,10 },		
+}
+
+max_pickups_set = 10 -- сколько нужно подобрать пикапов для окончания
+
+createdPickups = {}
+
+max_pickups = max_pickups_set
+if max_pickups_set > #pickupSpawns then
+	max_pickups = #pickupSpawns
+end
+
+function startWorking ()
+	if not getElementData ( localPlayer, "pizza_working" ) then
+		createdPickups = {}
+		triggerServerEvent ( "picku_pizza", localPlayer )
+		setElementData ( localPlayer, "pizza_working", true )
+		setElementData ( localPlayer, "pizza_working_hit", 0 )
+			outputChatBox ( "Вы начали работу.\nЕзжайте на пикап который указан на радаре!", 255, 255, 255, true )		
+				
+		for i, v in ipairs ( pickupSpawns ) do
+			local pick = createMarker ( v[1],v[2],v[3], "checkpoint", 4.0, 0, 0, 255 )
+			local pick2 = createPickup ( v[1],v[2],v[3], 3, 1582, 10000 )
+			setElementData ( pick, "taken", false )
+			setElementData ( pick, "pick2", pick2 )
+			setElementData ( pick, "pizza_pickup", true )
+			setElementData ( pick, "id", i )
+			if i == 1 then
+				local icon = createBlipAttachedTo ( pick, 41, 2 )
+				setElementData ( pick, "icon", icon )
+			end
+			table.insert (createdPickups, pick)
+		end
+	end
+end	
+
+addEventHandler("onClientVehicleExit", getRootElement(),
+    function(thePlayer, seat)
+        if thePlayer == getLocalPlayer() then
+			if getElementData ( localPlayer, "pizza_working" ) then
+				for i, v in ipairs ( createdPickups ) do
+					if isElement ( v ) then
+						local pick2 = getElementData ( v, "pick2")
+						if isElement ( pick2 ) then
+							destroyElement ( pick2 )
+						end
+						local icon = getElementData ( v, "icon" )
+						if isElement ( icon ) then
+							destroyElement ( icon )
+						end
+						destroyElement ( v )
+					end
+				end
+				triggerServerEvent ( "finitoWork_pizza", localPlayer )
+				setElementData ( localPlayer, "pizza_working", false )
+				outputChatBox ( "Вы закончили работу", 255, 255, 255, true )
+			end
+        end
+    end
+)
+
+function clientPickupHit(thePlayer, matchingDimension)
+	if thePlayer == localPlayer and getElementData ( source, "pizza_pickup" ) and not getElementData ( source, "taken" ) then
+		setElementData ( source, "taken", true )
+					setElementVelocity ( getPedOccupiedVehicle(localPlayer), 0, 0, 0 )
+					toggleAllControls ( false, true, false )
+					setTimer ( finishBoarding, 5000, 1 )
+					outputChatBox("Идёт доставка", 0,153,51)
+					setElementData ( localPlayer, "pizza_pickup", 2 )
+		local hitted = getElementData ( localPlayer, "pizza_working_hit" ) or 0
+		hitted = hitted+1
+		local pick2 = getElementData ( source, "pick2" )
+		if isElement ( pick2 ) then
+			destroyElement ( pick2 )
+		end
+		local icon = getElementData ( source, "icon" )
+		if isElement ( icon ) then
+			destroyElement ( icon )
+		end
+		destroyElement(source)
+		setElementData ( localPlayer, "pizza_working_hit", hitted)
+		if hitted >= max_pickups then
+			for i, v in ipairs ( createdPickups ) do
+				if isElement ( v ) then
+					local pick2 = getElementData ( v, "pick2")
+					if isElement ( pick2 ) then
+						destroyElement ( pick2 )
+					end
+					local icon = getElementData ( v, "icon" )
+					if isElement ( icon ) then
+						destroyElement ( icon )
+					end
+					destroyElement ( v )
+				end
+			end
+			setElementData ( localPlayer, "pizza_working", false )
+			triggerServerEvent ( "finitoWork_pizza", localPlayer )
+			outputChatBox ( "Вы закончили работу,подождите", 255, 255, 255, true )
+			return true
+		end
+		if createdPickups[hitted+1] then
+			local icon = createBlipAttachedTo ( createdPickups[hitted+1], 41, 2 )
+			setElementData ( createdPickups[hitted+1], "icon", icon )
+		end
+	end
+end
+
+function finishBoarding ()
+	toggleAllControls ( true, true, true )
+			outputChatBox ( "вы развезвезли пиццу,ваше вознаграждение - 500$ ", 255, 100, 100, true )
+			triggerServerEvent ( "giveMoneyFromClient_pizza", localPlayer, 500 ) --НАГРАДА ЗА ОСМОТР
+				setElementPosition ( trailer, bases[getElementData ( localPlayer, "nearestBase" )][1], bases[getElementData ( localPlayer, "nearestBase" )][2], bases[getElementData ( localPlayer, "nearestBase" )][3] )
+end
+addEventHandler ( "onClientMarkerHit", getRootElement(), clientPickupHit )
+
+
+
+
+
+
+
